59a356da08c9648623e3057c9092560286a4c533
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2024-09-19 17:28:22 -0400

    Update and rename PP.py to PP1_5.py

---
+++ PP1_5.py
@@ -0,0 +1,24 @@
+
+def q1():
+  #Write Assignment code here
+
+def q2():
+  #Write Assignment code here
+
+def q3():
+  #Write Assignment code here
+
+def q4():
+  #Write Assignment code here
+
+def q5():
+  #Write Assignment code here
+
+#Comment this code out when running tests
+#Do not comment this out when running your program normally
+
+q1()
+q2()
+q3()
+q4()
+q5()
